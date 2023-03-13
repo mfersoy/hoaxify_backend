d796838fcbb7a698a545884d904c9d18a4543c2a
Muhammed Fatih Ersoy <108956388+mfersoy@users.noreply.github.com>
2023-03-13 07:08:10 +0300

    usernameDomain_updated_with_annotation

--- src/main/java/com/hoaxify/domain/User.java
+++ src/main/java/com/hoaxify/domain/User.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -23,11 +25,16 @@ public class User  {
     private Long id;
 
     @Column
+    @Size(min = 3, max= 100)
     private String username;
 
     @Column
+    @Size(min = 3, max= 100)
     private String nickname;
+
     @Column
+    @Pattern(regexp ="^(?=.[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "Lütfen büyük küçük harf içeren bir password yazın")
+    @Size(min = 3, max= 100, message = "Password 30 karakterden daha az olamaz")
     private String password;
     @Column
     private String passwordRepeat;
